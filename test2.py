8e24731b172e51dc72e9331443e0c8c34a9c45c3
程卓 <chengzhuo@chengzhuodeMacBook-Pro.local>
2017-11-25 14:54:21 +0800

    polt set colour ,title,'x'and'y'size,moduler of point

---
+++ test2.py
@@ -0,0 +1,17 @@
+
+#-*- coding:utf-8 _*-  
+""" 
+@author:chengzhuo 
+@file: test2.py 
+@time: 2017/11/25 
+""" 
+import tensorflow as tf
+import pandas as pd 
+import math 
+import numpy as np
+from matplotlib import pyplot as plt
+plt.plot([6,3,4,0])
+plt.show()
+if __name__== '__main__':
+    pass
+
